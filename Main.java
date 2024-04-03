f7d95428faf995f076e7db6fc12edbc59ba8d8be
iabvad <dbaimanage@gmail.com>
2024-04-03 16:04:38 -0400

    h

--- Main.java
+++ Main.java
@@ -4,15 +4,11 @@ public class Main{
         Board board = new Board();
         board.newBoard();
 
+        board.showBoard();
         Board.board[1][0].move(3,0);
+        board.showBoard();
+
         Board.board[0][0].move(1, 0);
-        board.showBoard();
-
-        Board.board[3][0].move(4,0);
-        board.showBoard();
-
-        Board.board[1][0].move(3,0);
-        board.showBoard();
-
+        board.showBoard ();
             }
         }
--- Rook.java
+++ Rook.java
@@ -1,10 +1,10 @@
-public class Rook extends Piece{
+public class Rook extends Piece {
 
     private final String symbol;
 
-    public Rook(boolean isBlack, int row, int col){
+    public Rook(boolean isBlack, int row, int col) {
         super(isBlack, row, col);
-        if(this.isBlack) {
+        if (this.isBlack) {
             this.symbol = "♖";
         } else {
             this.symbol = "♜";
@@ -12,47 +12,38 @@ public class Rook extends Piece{
     }
 
     @Override
-    public boolean canMove ( int newRow, int newCol ) {
-        if (this.row == newRow) {
-            if (newCol > this.col) {
-                for (int i = this.col; i < newCol; i++) {
-                    if (Board.board[this.row ][ i ] != null) {
-                        return false;
+    public boolean canMove(int newRow, int newCol) {
+        if (newRow == this.row || newCol == this.col) {
+            // Rooks can move along rows or columns
+            // Check if there are any pieces in the way
+            int minRow = Math.min(this.row, newRow);
+            int maxRow = Math.max(this.row, newRow);
+            int minCol = Math.min(this.col, newCol);
+            int maxCol = Math.max(this.col, newCol);
+
+            // Check for obstruction along rows
+            if (newRow == this.row) {
+                for (int c = minCol + 1; c < maxCol; c++) {
+                    if (Board.board[newRow][c] != null) {
+                        return false; // There's a piece in the way
                     }
                 }
-                return true;
-            } else if (newCol < this.col) {
-                for (int i = this.col; i >= newCol; i--) {
-                    if (Board.board[ this.row ][ i ] != null) {
-                        return false;
-                    }
-                }
-                return true;
             }
-        } else if (this.col == newCol) {
-            if (newRow > this.row) {
-                for (int i = this.row; i < newRow; i++) {
-                    if (Board.board[ this.col ][ i ] != null) {
-                        return false;
+            // Check for obstruction along columns
+            if (newCol == this.col) {
+                for (int r = minRow + 1; r < maxRow; r++) {
+                    if (Board.board[r][newCol] != null) {
+                        return false; // There's a piece in the way
                     }
                 }
-                return true;
-            } else if (newRow < this.row) {
-                for (int i = this.row; i >= newRow; i--) {
-                    if (Board.board[ this.col ][ i ] != null) {
-                        return false;
-                    }
-                }
-                return true;
             }
-            return true;
+            return true; // No pieces in the way
         }
-        return false;
+        return false; // Rook can't move diagonally
     }
 
-
-
-    public String getSymbol(){
+    @Override
+    public String getSymbol() {
         return this.symbol;
     }
 }
